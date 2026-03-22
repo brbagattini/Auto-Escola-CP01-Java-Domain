29d423dddd7422017e0141cfe098264d2a60c6dc
pedro marchese <pedro.marchese03@gmail.com>
2026-03-22 18:25:53 -0300

    Fix: inumeras correções gerais que não tem como escrever em um commit

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ import models.Veiculo;
 public class Main {
     public static void main(String[] args){
         //iniciar Escola
+        System.out.println("Bem vindo a sistema da auto escola");
         AutoEscola autoEscola = new AutoEscola();
-
+        System.out.println("=============================");
         //criação de 4 alunos
-
         Aluno aluno1 = new Aluno();
         aluno1.nome = "juresvaldo";
         aluno1.idade = 18;
@@ -18,6 +18,9 @@ public class Main {
         aluno1.aprovado = false;
         aluno1.tipoVeiculo = "carro";
         autoEscola.adicionarAluno(aluno1);
+
+        System.out.println("=============================");
+
 
         Aluno aluno2 = new Aluno();
         aluno2.nome = "colosteu";
@@ -28,14 +31,20 @@ public class Main {
         aluno2.tipoVeiculo = "moto";
         autoEscola.adicionarAluno(aluno2);
 
+        System.out.println("=============================");
+
+
         Aluno aluno3 = new Aluno();
         aluno3.nome = "vlaumir";
         aluno3.idade = 19;
-        aluno3.aulasPraticas = 20;
-        aluno3.aulasTeoricas = 20;
+        aluno3.aulasPraticas = 4;
+        aluno3.aulasTeoricas = 3;
         aluno3.aprovado = false;
         aluno3.tipoVeiculo = "moto";
         autoEscola.adicionarAluno(aluno3);
+
+        System.out.println("=============================");
+
 
         Aluno aluno4 = new Aluno();
         aluno4.nome = "Zézin";
@@ -45,17 +54,47 @@ public class Main {
         aluno4.aprovado = true;
         aluno4.tipoVeiculo = "carro";
         autoEscola.adicionarAluno(aluno4);
-        autoEscola.exibirResumo();
+
+        System.out.println("=============================");
+
 
         Instrutor dorisvaldo = new Instrutor();
         dorisvaldo.nome = "Dorisvaldo";
         dorisvaldo.categoria = "carro";
         dorisvaldo.experiencia = 37;
 
+
         Veiculo nissanR32 = new Veiculo();
         nissanR32.modelo = "nissan R32";
         nissanR32.tipo = "carro";
         nissanR32.placa = "FVS-3829";
+
+
+
+        //simular realização de aulas
+        aluno3.agendarAulaPratica();
+
+        System.out.println("=============================");
+
+        aluno3.agendarAulaTeorica();
+
+        System.out.println("=============================");
+
+        aluno2.cancelarAulaTeorica();
+
+        System.out.println("=============================");
+
+        aluno2.cancelarAulaPratica();
+
+        System.out.println("=============================");
+
+        aluno3.realizarExame();
+
+        System.out.println("=============================");
+
+        aluno3.agendarAulaTeorica();
+
+        System.out.println("=============================");
 
         aluno3.realizarExame();
     }
--- src/models/Aluno.java
+++ src/models/Aluno.java
@@ -12,32 +12,47 @@ public class Aluno {
 
     public void agendarAulaTeorica(){
         aulasTeoricas++;
+        System.out.println("Aluno:" + nome);
+        System.out.println("Você realizou +1 aula teorica total:"+ aulasTeoricas);
     }
     public void agendarAulaPratica(){
         aulasPraticas++;
+        System.out.println("Aluno:" + nome);
+        System.out.println("Você realizou +1 aula pratica total:"+ aulasTeoricas);
+
     }
     public void cancelarAulaTeorica(){
+        System.out.println("Aluno:" + nome);
         aulasTeoricas--;
+        System.out.println("Você cancelou 1 aula teorica total:"+ aulasTeoricas);
     }
     public void cancelarAulaPratica(){
+        System.out.println("Aluno:" + nome);
         aulasPraticas--;
+        System.out.println("Você cancelou 1 aula pratica total:"+ aulasPraticas);
     }
     public void realizarExame(){
-        if (aulasPraticas < 5 & aulasTeoricas < 5){
+        if (aulasPraticas < 5 || aulasTeoricas < 5){
+                System.out.println("Aluno:" + nome);
+                System.out.println("Não apto para realizar o exame, você precisa no minimo de 5 aulas teoricas e praticas");
 
-            if (aprovado){
-                System.out.println("Você ja foi aprovado");
-            }else{
-                System.out.println("Você precisa no minimo de 5 aulas teoricas e praticas");
-            }
 
         }else{
-            //realizar o exame
-            //não aprendemos random mas precisamos implementar este metodo logo
-            // utilizamos Random e random.nextboolean
-            Random random = new Random();
-            aprovado = random.nextBoolean();
-            System.out.println("Status de aprovação:" + aprovado);
+
+            if (aprovado) {
+                System.out.println("Aluno:" + nome);
+                System.out.println("Você ja foi aprovado");
+            }else{
+                //realizar o exame
+                //não aprendemos random mas precisamos implementar este metodo logo
+                // utilizamos Random e random.nextboolean
+                Random random = new Random();
+                aprovado = random.nextBoolean();
+                System.out.println("Aluno:" + nome);
+                System.out.println("Você realizou a prova:");
+                System.out.println("Status de aprovação:" + aprovado);
+            }
+
         }
     }
 
